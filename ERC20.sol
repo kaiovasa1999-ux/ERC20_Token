75ba54e73bc82f1e749dc7c6ef123dfec2282e81
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-05-07 19:14:50 +0300

    added more functionality to my ERC20

--- ERC20.sol
+++ ERC20.sol
@@ -6,9 +6,11 @@ import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
     string private _name;
-    string private _symbol;
+    string private _symbol; 
     uint256 private _totalSupply;
+    uint8 private _decimals;
 
+    event transfeEV(address indexed to, uint amount);
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint)) private _allowances;
 
@@ -19,12 +21,13 @@ contract ERC20 is IERC20 {
         return _symbol;
     }
 
-    function balanceOf(address account) public view virtual override returns (uint256) {
-        return _balances[account];
+    function balanceOf(address _owner) public view virtual override returns (uint256) {
+        return _balances[_owner];
     }
 
     function transfer(address to, uint value) public payable override returns (bool){
         require(_balances[to] >= value,"insuffisiont ammount");
+        emit transfeEV(to,value);
         _balances[to] += value;
 
         return true;
@@ -33,4 +36,13 @@ contract ERC20 is IERC20 {
     function totalSupply() public view returns (uint256){
         return _totalSupply;
     }
+
+    function decimals() public view returns (uint8){
+        return _decimals;
+    }
+
+    function tranferFrom(address from,address to, uint256 amount) external view returns(bool){
+
+    }
+
 }
--- IERC20.sol
+++ IERC20.sol
@@ -6,6 +6,8 @@ interface IERC20{
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function totalSupply() external view returns (uint256);
+    function decimals() public view returns (uint8);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to,uint value) external payable returns (bool);
+    function tranferFrom(address from,address to, uint256 amount) external view returns(bool);
 }
